995b9cf9c9ca1b56b8b3def0c3f0e69a9bd453ec
sukkkuuuu <sunavy900@gmail.com>
2024-05-25 05:04:48 +0900

    검색 기능 구현

--- farmpedia/lib/screens/home_screen.dart
+++ farmpedia/lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../widgets/custom_searchbar_widget.dart';
+
 class HomeScreen extends StatefulWidget {
   final String id;
   const HomeScreen({super.key, required this.id});
@@ -12,8 +14,16 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   late SharedPreferences prefs;
   late List<String> userId;
+
+  // Search 기능을 로컬에서 확인하기
+  List<String> allItems = ["사과", "바나나", "체리", "밀", "포도", "보리", "사실"];
+  List<String> filteredItems = [];
+  // 사용자의 입력값을 확인함
+  TextEditingController searchController = TextEditingController();
+  bool onTextField = false;
+
   // 사용자의 개인 Id를 로컬 저장소에 저장한것
-  Future<void> initPrefs() async {
+  Future initPrefs() async {
     prefs = await SharedPreferences.getInstance();
     userId = prefs.getStringList('userId') ?? [];
     if (userId.isEmpty) {
@@ -26,6 +36,45 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     super.initState();
     initPrefs();
+
+    // Search 기능
+    filteredItems = allItems;
+    // 사용자가 입력하면
+    searchController.addListener(() {
+      setState(() => onTextField = true);
+      // 입력값을 filterSearchResults로 보냄
+      filterSearchResults(searchController.text);
+    });
+  }
+
+  // 사용자가 입력한 단어와 같은 단어가 있는지 확인
+  void filterSearchResults(String query) {
+    if (query.isNotEmpty) {
+      List<String> dummyListData = [];
+      for (var item in allItems) {
+        // 단어를 영어를 예로 들었기 떄문에 이렇게 사용했던거임
+        // 리스트 단어를 한글로 수정함 -> 나중에 수정 필요
+        if (item.toLowerCase().contains(query.toLowerCase())) {
+          dummyListData.add(item);
+        }
+      }
+      setState(() {
+        // 해당 단어를 포함한 단어 리스트를 갱신
+        filteredItems = dummyListData;
+      });
+    } else {
+      setState(() {
+        // 단어 입력이 없으면 공백으로 갱신
+        filteredItems = [];
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    // 삭제 기능
+    searchController.dispose();
+    super.dispose();
   }
 
   @override
@@ -49,38 +98,16 @@ class _HomeScreenState extends State<HomeScreen> {
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: Column(
             children: [
-              Container(
-                decoration: BoxDecoration(
-                  color: mainColor,
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 3, horizontal: 20),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: "Search",
-                            hintStyle: TextStyle(
-                              color: Colors.black,
-                            ),
-                            suffixIcon: Icon(
-                              Icons.search_outlined,
-                              color: Colors.black,
-                            ),
-                            border: InputBorder.none,
-                            contentPadding: EdgeInsets.symmetric(
-                              vertical: 14.0,
-                              horizontal: 10,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+              // 길어서 따로 위젯으로 뺌
+              Expanded(
+                child: CustomSearch(
+                  mainColor: mainColor,
+                  searchController: searchController,
+                  filteredItems: filteredItems,
+                  onTextField: onTextField,
                 ),
               ),
+              Container(),
             ],
           ),
         ),
---
+++ farmpedia/lib/widgets/custom_searchbar_widget.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class CustomSearch extends StatelessWidget {
+  const CustomSearch({
+    super.key,
+    required this.mainColor,
+    required this.searchController,
+    required this.filteredItems,
+    required this.onTextField,
+  });
+
+  final Color mainColor;
+  final TextEditingController searchController;
+  final List<String> filteredItems;
+  final bool onTextField;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: mainColor,
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 20),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: searchController,
+                    autocorrect: false, // 자동 수정 비활성화
+                    textCapitalization: TextCapitalization.none,
+                    keyboardType: TextInputType.text, // 텍스트 입력 설정
+                    decoration: const InputDecoration(
+                      hintText: "Search",
+                      hintStyle: TextStyle(
+                        color: Colors.black,
+                      ),
+                      suffixIcon: Icon(
+                        Icons.search_outlined,
+                        color: Colors.black,
+                      ),
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: 14.0,
+                        horizontal: 10,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
+        // 사용자가 단어를 입력했거나 단어 리스트가 공백 아닐 때
+        // 입력된 단어의 리스트들을 보여줌
+        Expanded(
+          child: filteredItems.isNotEmpty && onTextField
+              ? ListView.builder(
+                  itemCount: filteredItems.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(filteredItems[index]),
+                    );
+                  },
+                )
+              : Container(),
+        ),
+      ],
+    );
+  }
+}
