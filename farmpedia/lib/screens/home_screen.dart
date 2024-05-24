72445e5b8c16c24fbc2a08a44b8140560d5a1804
sukkkuuuu <sunavy900@gmail.com>
2024-05-24 18:43:15 +0900

    home_screen 검색창 UI 추가

--- farmpedia/lib/screens/home_screen.dart
+++ farmpedia/lib/screens/home_screen.dart
@@ -5,15 +5,57 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const Color mainColor = Color(0xff95C461);
+    // const Color barColor = Color(0xffF1F1F1);
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
-          title: const Text('귀농백과'),
+          title: const Text(
+            '귀농백과',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          backgroundColor: Colors.white,
         ),
-        body: const Center(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: Column(
             children: [
-              Text('HOME'),
+              Container(
+                decoration: BoxDecoration(
+                  color: mainColor,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 3, horizontal: 20),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "Search",
+                            hintStyle: TextStyle(
+                              color: Colors.black,
+                            ),
+                            suffixIcon: Icon(
+                              Icons.search_outlined,
+                              color: Colors.black,
+                            ),
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.symmetric(
+                              vertical: 14.0,
+                              horizontal: 10,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
