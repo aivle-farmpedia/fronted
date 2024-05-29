f5b053b05f8e541d34cbc36745fb69950de76cdc
hyeonmil99 <namga71@naver.com>
2024-05-29 23:09:03 +0900

    policy 화면 수정

--- farmpedia/lib/screens/policy_screen.dart
+++ farmpedia/lib/screens/policy_screen.dart
@@ -21,22 +21,133 @@ class PolicyScreen extends StatelessWidget {
                   ),
                 );
               },
-              child: const Padding(
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                child: Icon(
-                  Icons.menu,
-                  size: 30.0,
-                ),
-              ),
+              child: const Icon(Icons.menu),
             ),
           ],
           backgroundColor: const Color.fromARGB(255, 241, 240, 240),
           title: const Text(
-            "귀농 정책 및 혜택",
+            "지원 정책",
             style: TextStyle(
               fontSize: 25,
               fontWeight: FontWeight.w800,
             ),
+          ),
+        ),
+        body: const Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                "귀농 지원",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w800),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 50),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SupportCard(
+                      title: '롯데가 언제나 승리한다',
+                      subtitle: '롯데한국시리즈',
+                      color: Color.fromARGB(255, 188, 237, 131),
+                      icon: Icons.account_circle,
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    SupportCard(
+                      title: '롯데가 언제나 승리한다',
+                      subtitle: '최강 롯데',
+                      color: Colors.lightGreen,
+                      icon: Icons.account_circle,
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "혜택",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.w800),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        SizedBox(
+                          height: 25,
+                        ),
+                        SupportCard(
+                          title: '엔씨 다이노스',
+                          subtitle: '이기자',
+                          color: Color.fromARGB(255, 188, 237, 131),
+                          icon: Icons.account_circle,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    SupportCard(
+                      title: '롯데 한국 시리즈',
+                      subtitle: '최강 롯데',
+                      color: Colors.lightGreen,
+                      icon: Icons.account_circle,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SupportCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final Color color;
+  final IconData icon;
+  final double widthFactor;
+
+  const SupportCard({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.color,
+    required this.icon,
+    this.widthFactor = 1.35,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: widthFactor,
+      child: Card(
+        color: color,
+        child: ListTile(
+          leading: Icon(
+            icon,
+            size: 40,
+          ),
+          title: Text(
+            title,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          subtitle: Text(
+            subtitle,
           ),
         ),
       ),
