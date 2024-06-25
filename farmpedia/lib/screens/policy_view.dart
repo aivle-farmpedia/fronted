7f7cd334d00532bbcd1ab3f93a5092db4a8dc83d
hyeonmil99 <namga71@naver.com>
2024-06-25 22:51:24 +0900

    가격 단위 수정

--- farmpedia/lib/screens/policy_view.dart
+++ farmpedia/lib/screens/policy_view.dart
@@ -1,17 +1,49 @@
 import 'package:farmpedia/models/support_policy.dart';
+import 'package:farmpedia/widgets/web_view_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart'; // intl 패키지 import/ WebViewScreen 위젯 import
 
 class PolicyView extends StatelessWidget {
   final SupportPolicy supportPolicy;
 
   const PolicyView({super.key, required this.supportPolicy});
 
+  String replaceSpecialCharacters(String content) {
+    return content
+        .replaceAll('○', '\n○')
+        .replaceAll('ㅇ', '\nㅇ')
+        .replaceAll('?', ''); // '는 공백 대신 제거
+  }
+
+  void _openWebView(BuildContext context, String url) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => WebViewScreen(url: url),
+      ),
+    );
+  }
+
+  String formatPrice(String price) {
+    try {
+      final intPrice = int.parse(price);
+      final NumberFormat numberFormat = NumberFormat('#,###');
+      return numberFormat.format(intPrice);
+    } catch (e) {
+      // 오류가 발생할 경우 원래 문자열을 반환
+      return price;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // Split the content at 'o' and then join it back with '\n○' to ensure new lines
-    String contentWithNewLines = supportPolicy.content.split('○').join('\n○');
+    String contentWithNewLines =
+        replaceSpecialCharacters(supportPolicy.content);
     List<String> contentParagraphs = contentWithNewLines.split('\n');
+
+    // 천단위로 쉼표 추가
+    String formattedPrice = formatPrice(supportPolicy.price.toString());
 
     return Scaffold(
       appBar: AppBar(
@@ -44,9 +76,18 @@ class PolicyView extends StatelessWidget {
             const SizedBox(height: 10),
             Text('교육 대상: ${supportPolicy.eduTarget}'),
             const SizedBox(height: 10),
-            Text('가격: ${supportPolicy.price}'),
+            Text('가격: $formattedPrice원'),
             const SizedBox(height: 10),
-            Text('정보 URL: ${supportPolicy.infoUrl}'),
+            GestureDetector(
+              onTap: () => _openWebView(context, supportPolicy.infoUrl),
+              child: Text(
+                '정보 URL: ${supportPolicy.infoUrl}',
+                style: const TextStyle(
+                  color: Colors.blue,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
           ],
         ),
       ),
---
+++ farmpedia/lib/widgets/web_view_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewScreen extends StatelessWidget {
+  final String url;
+
+  const WebViewScreen({super.key, required this.url});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('WebView'),
+      ),
+      body: WebView(
+        initialUrl: url,
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
